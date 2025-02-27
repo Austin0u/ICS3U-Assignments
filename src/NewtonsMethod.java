65f97979a4ec6448a740762d71a5d169b37e9f0f
Austin <austin.ou@student.tdsb.on.ca>
2025-02-27 18:25:40 -0500

    finished newtons method assignment

--- src/NewtonsMethod.java
+++ src/NewtonsMethod.java
@@ -3,42 +3,40 @@ import java.util.Scanner;
 public class NewtonsMethod {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        char response;
+        char response = 'y'; // Defaults to y for first interation
 
-        // Loop to calculate
-        do {
-            // Prompts user for an n value
+        while (response == 'y') { 
+            // Prompts user for an n value (initial guess)
             System.out.print("Please enter an n value: ");
-            double guess = keyboard.nextDouble();
-            double x = guess;
-            double x1 = 0;
+            double x = keyboard.nextDouble();
 
-            // Continues to guess to obtain a root is found
-            search:
-            while (x1 != -1) {
-                // Calculates and prints the computer guess
-                x1 = x - (6 * Math.pow(x, 4) - 13 * Math.pow(x, 3) - 18 * Math.pow(x, 2) + 39 * x + 6) / (24 * Math.pow(x, 3) - 39 * Math.pow(x, 2) - 36 * x + 7);
+            // Uses Newton's method to find roots
+            while (true) {
+                // Calculates the function and its derivative
+                double fx = 6 * Math.pow(x, 4) - 13 * Math.pow(x, 3) - 18 * Math.pow(x, 2) + 7 * x + 6;
+                double fpx = 24 * Math.pow(x, 3) - 39 * Math.pow(x, 2) - 36 * x + 7;
 
-                for (int i = 0; i <= 4, i++) {
-                    double[] roots = {-1,-1/2,1/3, 3};
-                    double difference = Math.abs(x - root[i]);
-                    if (difference <= 0.01) {
-                        break sesrch;
-                    }
+                // Calculates the next (computer) guess using function and prints the successive aproximations
+                double x1 = x - fx / fpx;
+                System.out.println(x1);
+
+                // Breaks from loop once the guesses are close enough
+                if (Math.abs(x1 - x) < 0.001) {
+                    break;
                 }
 
-                x = x1;
-                System.out.println(x1);
+                x = x1; // set x to the new guess for the next iteration
             }
 
             // Prints the final quess
-            System.out.println("The root is at x = " + x1);
+            System.out.println("The root is at " + x);
 
-            // Prompts user if they want to repeat the calculation
+            // Prompts user if they want to retry
             System.out.print("Do you to try again? (y/n): ");
             response = keyboard.next().toLowerCase().charAt(0);
-        } while (response == 'y'); // If the user says yes, do again, else break
+        } 
 
         keyboard.close();
+
     }
 }
