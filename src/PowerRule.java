c3c1373b477cdcc4e16a9e8bab90e8412d263008
Ou <Austin.Ou@student.tdsb.on.ca>
2025-03-18 10:44:43 -0400

    edited comments on power rule

--- src/PowerRule.java
+++ src/PowerRule.java
@@ -15,17 +15,13 @@ public class PowerRule {
 
         // Calculate both first and second derivatives in the same loop
         for (int i = 0; i < coefficients.length; i++) {
-            // Local variables to store the original double and int versions of the coefficient and exponent (to be modified later)
-            // double coef = Double.parseDouble(coefficients[i]);
-            // int exp = Integer.parseInt(exponents[i]);
-
             // Calculates the first derivative
             double firstCoef = Double.parseDouble(coefficients[i]) * Integer.parseInt(exponents[i]);
             int firstExp = Integer.parseInt(exponents[i]) - 1;
 
             if (firstCoef != 0) {
                 String term = firstCoef + ((firstExp != 0) ? "x^" + firstExp : ""); // Concatenates the term, only adds exponent if needed
-                firstDerivative += (firstCoef > 0 && i != 0 ? " +" : " ") + term; // Adds a plus sign if needed
+                firstDerivative += (firstCoef > 0 && i != 0 ? " +" : " ") + term; // Adds to derivative, only adds + if needed
             }
 
             // Calculates the second derivative
@@ -34,7 +30,7 @@ public class PowerRule {
 
             if (secondCoef != 0) {
                 String term = secondCoef + ((secondExp != 0) ? "x^" + secondExp : ""); // Concatenates the term, only adds exponent if needed
-                secondDerivative += (secondCoef > 0 && i != 0 ? " +" : " ") + term; // Adds a plus sign if needed
+                secondDerivative += (secondCoef > 0 && i != 0 ? " +" : " ") + term; // Adds to derivative, only adds + if needed
             }
         }
 
