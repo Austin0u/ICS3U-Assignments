558533839abba24731db496c9deb025f8143462b
Austin <austin.ou@student.tdsb.on.ca>
2025-03-18 20:25:21 -0400

    polishing up newtons method

--- src/NewtonsMethod.java
+++ src/NewtonsMethod.java
@@ -3,9 +3,8 @@ import java.util.Scanner;
 public class NewtonsMethod {
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-        char response = 'y'; // Allows first iteration
 
-        while (response == 'y') {
+        while (true) {
             // Prompt user for an initial guess
             System.out.print("Please enter an initial guess: ");
             double x = keyboard.nextDouble();
@@ -20,21 +19,21 @@ public class NewtonsMethod {
                 double x1 = x - fx / fpx;
                 System.out.println(x1);
 
-                // Break from loop once the guesses are close enough
+                // Break from loop once the guess is close enough
                 if (Math.abs(x1 - x) < 0.000000001) {
-                    x = x1; // Ensure it will output the last guess
+                    // Print the final guess
+                    System.out.println("The root is at " + x1);
                     break;
                 }
 
                 x = x1; // Set x to the new guess for the next iteration
             }
 
-            // Print the final guess
-            System.out.println("The root is at " + x);
-
             // Prompt user if they want to retry
             System.out.print("Do you want to try again? (y/n): ");
-            response = keyboard.next().toLowerCase().charAt(0);
+            if (keyboard.next().toLowerCase().charAt(0) != 'y') {
+                break;
+            }
         }
 
         keyboard.close();
