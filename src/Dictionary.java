abba60cb4e6fd3c703c271f387840893dd1bfd80
Austin <austin.ou@student.tdsb.on.ca>
2025-03-27 23:56:57 -0400

    finished assignment 8

---
+++ src/Dictionary.java
@@ -0,0 +1,49 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.HashSet;
+import java.util.Scanner;
+
+public class Dictionary {
+    public static void main(String[] args) throws Exception {
+        BufferedReader inputStream = null;
+        HashSet<String> dictionary = new HashSet<String>();
+        Scanner keyboard = new Scanner(System.in);
+        // StringTokenizer st = null;
+        String line = "";
+
+        try {
+            // Read file and adds words to the dictionary hashset
+            inputStream = new BufferedReader(new FileReader("dictionary.txt"));
+
+            while ((line = inputStream.readLine()) != null) {
+                dictionary.add(line);
+            }
+
+            // Prompt user for sentence input
+            System.out.println("Please enter a sentence: ");
+            String sentence[] = keyboard.nextLine().split(" ");
+            keyboard.close();
+
+            // Spell check
+            System.out.println("\nSpell check of your sentence:");
+
+            for (int i = 0; i < sentence.length; i++) {
+                if (dictionary.contains(sentence[i].toLowerCase())) {
+                    System.out.println((i + 1) + ". " + sentence[i] + " <valid>");
+                } else {
+                    System.out.println((i + 1) + ". " + sentence[i] + " <invalid>");
+                }
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Error opening file");
+        } catch (IOException e) {
+            System.out.println("Error creating file");
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+    }
+}
