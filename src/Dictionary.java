5fbb5fac4693fe72f9e20511c32db5243d2df666
unknown <342906989@TDSB.ON.CA>
2025-03-28 11:40:15 -0400

    tried

--- src/Dictionary.java
+++ src/Dictionary.java
@@ -4,13 +4,13 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.HashSet;
 import java.util.Scanner;
+import java.lang.StringBuilder;
 
 public class Dictionary {
     public static void main(String[] args) throws Exception {
         BufferedReader inputStream = null;
         HashSet<String> dictionary = new HashSet<String>();
         Scanner keyboard = new Scanner(System.in);
-        // StringTokenizer st = null;
         String line = "";
 
         try {
@@ -18,13 +18,31 @@ public class Dictionary {
             inputStream = new BufferedReader(new FileReader("dictionary.txt"));
 
             while ((line = inputStream.readLine()) != null) {
-                dictionary.add(line);
+                dictionary.add(line.toLowerCase());
             }
 
             // Prompt user for sentence input
             System.out.println("Please enter a sentence: ");
             String sentence[] = keyboard.nextLine().split(" ");
             keyboard.close();
+
+            // Removes punctuation
+            for (int i = 0;  i < sentence.length; i++) {
+                StringBuilder sb = new StringBuilder(sentence[i]);
+                System.out.println("Word:" + sb.toString());
+
+                for (int j = 0; j < sb.length(); j++) {
+                    char character = sb.charAt(j);
+                    System.out.println("> Char " + j + ": " + character);
+
+                    if ((!(character >= 'A' && character <= 'Z') && !(character >= 'a' && character <= 'z'))) {
+                        sb.deleteCharAt(j);
+                        System.out.println("   > Deleted " + character);
+                    }                    
+                }
+
+                sentence[i] = sb.toString();
+            }
 
             // Spell check
             System.out.println("\nSpell check of your sentence:");
