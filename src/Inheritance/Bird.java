4db459c17df1df480c178db45127af62bd7b0dc4
unknown <342906989@TDSB.ON.CA>
2025-04-29 11:37:50 -0400

    began implementing javadocs

--- src/Inheritance/Bird.java
+++ src/Inheritance/Bird.java
@@ -1,5 +1,13 @@
 package Inheritance;
 
+/**
+ * The Bird class is superclass that is to be
+ * inherited by other subclasses, such as "Waterfowl"
+ * and "Duck".
+ * 
+ * @author Austin Ou
+ * @version 1.0
+ */
 public class Bird {
     protected String name;
     protected int age;
@@ -8,7 +16,14 @@ public class Bird {
     protected int eggCount;
     protected String noise;
 
-    // Constructors
+    /**
+     * Constructs a new Bird object with the given parameters.
+     * 
+     * @param name - the name of the bird 
+     * @param age - the age of the bird
+     * @param canFly - whether the bird can fly
+     * @param noise - the noise the bird makes
+     */
     public Bird(String name, int age, boolean canFly, String noise) {
         this.name = name;
         this.age = age;
@@ -18,60 +33,112 @@ public class Bird {
         this.noise = noise;
     }
 
+    /**
+     * Initalizes a new Bird object with only the name and age.
+     * 
+     * @param name - the name of the bird 
+     * @param age - the age of the bird
+     */
     public Bird(String name, int age) {
         this(name, age, true, "chirp");
     }
 
+    /**
+     * Initalizes a new Bird object with only the name.
+     * 
+     * @param name - the name of the bird 
+     */
     public Bird(String name) {
         this(name, 0, true, "chirp");
     }
 
-    // Getters and Setters
+    /**
+     * @return the name
+     */
     protected String getName() {
         return name;
     }
 
+    /**
+     * @return the age
+     */
     protected int getAge() {
         return age;
     }
 
+    /**
+     * Determines if the bird is capable of flying.
+     * 
+     * @return true if the bird can fly, false otherwise
+     */
     protected boolean canFly() {
         return canFly;
     }
 
+    /**
+     * @return the number of eggs laid
+     */
     protected int eggCount() {
         return eggCount;
     }
 
+    /**
+     * Sets the name
+     * 
+     * @param name - the new name
+     */
     protected void setName(String name) {
         this.name = name;
     }
 
+    /**
+     * Sets the age.
+     * 
+     * @param age - the new age
+     */
     protected void setAge(int age) {
         this.age = age;
     }
 
+    /**
+     * Determines if the bird is currently in the air
+     * 
+     * @return true if the bird is currently in the air, false otherwise
+     */
     protected boolean isInAir() {
         return isFlying;
     }
 
-    // Instance Methods
+    /**
+     * Method to lay an egg; Increases the number of eggs.
+     * Also prints a message stating who laid the egg.
+     */
     protected void layEgg() {
         eggCount++;
         System.out.println(name + " has laid an egg");
     }
 
+    /**
+     * Starts flight. Will only fly if the bird is capable of flight.
+     * Also prints a message stating who has begun flying.
+     */
     protected void startFlying() {
         if (canFly) {
-            isFlying = true;
-            System.out.println(name + " has started flying");
+            if (!isFlying) {
+                isFlying = true;
+                System.out.println(name + " has started flying");
+            } else {
+                System.out.println(name + " is already flying");
+            }
         }
     }
 
+    /**
+     * Ends flight (lands). Will only fly if the bird is capable of flight.
+     * Also prints a message stating who has stopped flying.
+     */
     protected void stopFlying() {
-        if (!canFly) {
-            System.out.println(name + " cannot fly");
-        } else if (isFlying) {
+        if (isFlying) {
             isFlying = false;
             System.out.println(name + " has stopped flying");
         } else {
@@ -79,14 +146,29 @@ public class Bird {
         }
     }
 
+    /**
+     * Determines if this bird is older than the referenced bird.
+     * 
+     * @param bird - the other bird that is being compared
+     * @return true if this bird is older, false otherwise
+     */
     protected boolean isOlder(Bird bird) {
         return (this.age > bird.getAge()) ? true : false;
     }
 
+    /**
+     * Determines if this bird has the same name as the referenced bird.
+     * 
+     * @param bird - the other bird that is being compared
+     * @return true if both have the same name, false otherwise
+     */
     protected boolean sameName(Bird bird) {
         return this.name.equalsIgnoreCase(bird.getName());
     }
 
+    /**
+     * Prints a message of the noise this bird creates.
+     */
     protected void makeNoise() {
         System.out.println(this.name + " " + this.noise + "s");
     }
--- src/Inheritance/Duck.java
+++ src/Inheritance/Duck.java
@@ -1,10 +1,27 @@
 package Inheritance;
 
+/**
+ * The Duck class is a subclass of Waterfowl.
+ * 
+ * @author Austin Ou
+ * @version 1.0
+ */
 public class Duck extends Waterfowl {
     protected String breed; // e.g., "mallard", "pekin", "cayuga", "crested"
     protected String pattern; // e.g., "spotted", "solid", "striped"
     protected String patternColour;
 
+    /**
+     * Constructs a new Duck object with the given parameters.
+     * 
+     * @param name - the name of the bird 
+     * @param age - the age of the bird
+     * @param canFly - whether the bird can fly
+     * @param canSwim - whether the bird can swim
+     * @param waterTypeTolerance - the water tolerance of the bird (e.g. saltwater or freshwater)
+     * @param pattern - the type of pattern 
+     * @param patternColour - the colour of the pattern
+     */
     public Duck(String name, int age, boolean canFly, boolean canSwim, String breed, String waterTypeTolerance, String pattern, String patternColour) {
         super(name, age, canFly, canSwim, true, "quack", waterTypeTolerance);
         this.breed = breed;
@@ -12,6 +29,9 @@ public class Duck extends Waterfowl {
         this.patternColour = patternColour;
     }
     
+    /**
+     * Initalizes a new Duck object with name, age, breed, water tollerance, patterm, and pattern colour.
+     */
     public Duck(String name, int age, String breed, String waterTypeTolerance, String pattern, String patternColour) {
         this(name, age, true, true, breed, waterTypeTolerance, pattern, patternColour);
     }
