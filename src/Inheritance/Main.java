65af176573b898fe47889bfaec88fa86feee4629
Austin <austin.ou@student.tdsb.on.ca>
2025-05-21 02:06:56 -0400

    Checked

--- src/Inheritance/Main.java
+++ src/Inheritance/Main.java
@@ -5,7 +5,7 @@ public class Main {
         // Test Bird class
         System.out.println("======== Bird Class Test ========");
         Bird bird = new Bird("Sparrow", 2);
-        System.out.println("> Bird: " + bird.getName());
+        System.out.println("> Name: " + bird.getName());
         System.out.println("> Age: " + bird.getAge());
         System.out.println("> Can Fly: " + bird.canFly());
         System.out.println("---------------------------------");
@@ -18,7 +18,7 @@ public class Main {
         // Test Waterfowl class
         System.out.println("\n====== Waterfowl Class Test ======");
         Waterfowl swan = new Waterfowl("Chas the Swan", 3, true, true, true, "honk", "freshwater");
-        System.out.println("> Waterfowl: " + swan.getName());
+        System.out.println("> Name: " + swan.getName());
         System.out.println("> Age: " + swan.getAge());
         System.out.println("> Can Fly: " + swan.canFly());
         System.out.println("> Can Swim: " + swan.canSwim());
@@ -34,21 +34,23 @@ public class Main {
         Duck duck1 = new Duck("Alice", 4, true, true, "crested", "freshwater", "spotted", "brown");
         Duck duck2 = new Duck("Bob", "crested", "freshwater", "solid", "white");
 
-        System.out.println("> Duck 1: " + duck1.getName());
-        System.out.println("> Age: " + duck1.getAge());
-        System.out.println("> Can Fly: " + duck1.canFly());
-        System.out.println("> Can Swim: " + duck1.canSwim());
-        System.out.println("> Breed: " + duck1.getBreed());
-        System.out.println("> Pattern: " + duck1.getPattern());
-        System.out.println("> Pattern Colour: " + duck1.getPatternColour());
+        System.out.println("> Duck 1:");
+        System.out.println("  > Name: " + duck1.getName());
+        System.out.println("  > Age: " + duck1.getAge());
+        System.out.println("  > Can Fly: " + duck1.canFly());
+        System.out.println("  > Can Swim: " + duck1.canSwim());
+        System.out.println("  > Breed: " + duck1.getBreed());
+        System.out.println("  > Pattern: " + duck1.getPattern());
+        System.out.println("  > Pattern Colour: " + duck1.getPatternColour());
 
-        System.out.println("\n> Duck 2: " + duck2.getName());
-        System.out.println("> Age: " + duck2.getAge());
-        System.out.println("> Can Fly: " + duck2.canFly());
-        System.out.println("> Can Swim: " + duck2.canSwim());
-        System.out.println("> Breed: " + duck2.getBreed());
-        System.out.println("> Pattern: " + duck2.getPattern());
-        System.out.println("> Pattern Colour: " + duck2.getPatternColour());
+        System.out.println("\n> Duck 2:");
+        System.out.println("  > Name: " + duck2.getName());
+        System.out.println("  > Age: " + duck2.getAge());
+        System.out.println("  > Can Fly: " + duck2.canFly());
+        System.out.println("  > Can Swim: " + duck2.canSwim());
+        System.out.println("  > Breed: " + duck2.getBreed());
+        System.out.println("  > Pattern: " + duck2.getPattern());
+        System.out.println("  > Pattern Colour: " + duck2.getPatternColour());
         System.out.println("---------------------------------");
 
         duck1.enterWater("freshwater");
@@ -61,13 +63,15 @@ public class Main {
 
         duck1.makeNoise();
         duck2.layEgg();
+
         System.out.println(
                 "> Is " + duck1.getName() + " the Same Breed as " + duck2.getName() + ": " + duck1.isSameBreed(duck2));
         System.out.println("> Do " + duck1.getName() + " and " + duck2.getName() + " Have the Same Pattern?: "
                 + duck1.hasSamePattern(duck2));
-        System.out.println("> Do " + duck1.getName() + " and " + duck2.getName() + " Have the Same Pattern colour?: "
+        System.out.println("> Do " + duck1.getName() + " and " + duck2.getName() + " Have the Same Pattern Colour?: "
                 + duck1.hasSamePatternColour(duck2));
 
+        System.out.println("\n=== Age Comparison ===");
         if (duck1.isOlder(duck2)) {
             System.out.println("> " + duck1.getName() + " is older than " + duck2.getName());
         } else if (duck2.isOlder(duck1)) {
